a963f2a88e2cf02c565c0176918605a5cf169688
RyanSitorus <Andriansitorus1@gmail.com>
2026-09-12 19:31:29 +0700

    init commit

---
+++ src/main/java/com/assessment/product/ProductManagementApplication.java
@@ -0,0 +1,17 @@
+package com.assessment.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableCaching
+@EnableAsync
+public class ProductManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductManagementApplication.class, args);
+    }
+
+}
